394d72bf44a10a1bfdc66cf62627882114e6ea09
Ilia Bochkarev <ilia_bochkarev@epam.com>
2018-04-08 16:36:57 +0300

    logging added

--- src/main/java/tesei7/ms/agents/agent/HelloController.java
+++ src/main/java/tesei7/ms/agents/agent/HelloController.java
@@ -1,9 +1,11 @@
 package tesei7.ms.agents.agent;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
     @Value("${tesei7.agents.greeting}")
@@ -13,6 +15,7 @@ public class HelloController {
 
     @GetMapping("/greeting")
     public String getGreeting() {
+        log.debug("Hello service invoked");
         return String.format("%s, your password is %s", defaultGreeting, password);
     }
 }
